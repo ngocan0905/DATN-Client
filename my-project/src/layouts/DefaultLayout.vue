4f9011750ae68b2d2acc0df3f5d0b86506240883
ngocan0905 <anlecntt.0905@gmail.com>
2023-12-03 14:57:26 +0700

    create navbar

---
+++ my-project/src/layouts/DefaultLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <HeaderVue />
+    <slot></slot>
+  </div>
+</template>
+<script setup>
+import HeaderVue from '../components/HeaderVue.vue'
+</script>
--- my-project/src/views/HomeView.vue
+++ my-project/src/views/HomeView.vue
@@ -1,16 +1,8 @@
-<script setup></script>
-
 <template>
-  <main class="flex justify-end">
-    <button
-      class="px-4 py-2 bg-gray-600 text-white hover:bg-gray-950 duration-300 hover:scale-105 m-4"
-      @click="generalStore.isLoginFormOpen = true"
-    >
-      login
-    </button>
-  </main>
+  <DefaultLayout>
+    <main class="flex justify-end"></main>
+  </DefaultLayout>
 </template>
 <script setup>
-import { useGeneralStore } from '../stores/counter.js'
-const generalStore = useGeneralStore()
+import DefaultLayout from '../layouts/DefaultLayout.vue'
 </script>
